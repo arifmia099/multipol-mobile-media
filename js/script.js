002e0d586bd179eb2115740a126b41fbfbbdabed
arif mia <142096189+arifmia099@users.noreply.github.com>
2023-11-26 12:10:50 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+
+	$('#btn').on( "click", function(){
+		alert("Choice Your Mobile Message For Inbox");
+	});
+	// counterUp
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+});
